2ce46da597e0f12fbfcdb95600a100e06226ba43
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2023-11-25 14:46:42 +0530

    StepBuilderFactory-practice

---
+++ springbatch/src/main/java/com/batch/config/SampleJob.java
@@ -0,0 +1,66 @@
+package com.batch.config;
+
+import com.batch.service.SecondTasklet;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SampleJob {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    private SecondTasklet secondTasklet;
+
+//    @Autowired
+//    private FirstJobListener firstJobListener;
+//
+//    @Autowired
+//    private FirstStepListener firstStepListener;
+
+    @Bean
+    public Job firstJob() {
+        return jobBuilderFactory
+                .get("First Job")
+                .start(firstStep())
+                .next(secondStep())
+                .build();
+    }
+
+    private Step firstStep() {
+        return stepBuilderFactory.get("First Step")
+                .tasklet(firstTask())
+                .build();
+    }
+    private Step secondStep() {
+        return stepBuilderFactory.get("Second Step")
+                .tasklet(secondTasklet)
+                .build();
+    }
+
+    private Tasklet firstTask() {
+        return new Tasklet() {
+
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("This is first tasklet step");
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+
+}
